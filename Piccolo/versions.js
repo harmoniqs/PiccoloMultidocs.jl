8d0aa0530a49e2dbe36b9e70378688a38580fe93
jack-champagne <jack-champagne@users.noreply.github.com>
2026-02-13 11:30:06 +0000

    deploy: 2d2e114de5bdfc1555186870db46b25420213461

--- Piccolo/versions.js
+++ Piccolo/versions.js
@@ -1,12 +1,13 @@
 var DOC_VERSIONS = [
+  "dev",
   "stable",
   "v1.1",
   "v1.0",
   "v0.11",
+  "v0.10",
   "v0.9",
   "v0.8",
   "v0.7",
-  "dev",
 ];
-var DOCUMENTER_NEWEST = "v1.1.1";
-var DOCUMENTER_STABLE = "stable";
+var DOCUMENTER_NEWEST = "dev";
+var DOCUMENTER_STABLE = "dev";
