ac36769785a7ca1efff3474abdc3eee596cc7b05
jack-champagne <jack-champagne@users.noreply.github.com>
2026-02-24 22:22:35 +0000

    deploy: 52cdc9376f25558aa954025ca25286705614f4fc

--- DirectTrajOpt/versions.js
+++ DirectTrajOpt/versions.js
@@ -1,8 +1,8 @@
 var DOC_VERSIONS = [
-  "dev",
   "stable",
   "v0.8",
   "v0.4",
+  "dev",
 ];
-var DOCUMENTER_NEWEST = "dev";
-var DOCUMENTER_STABLE = "dev";
+var DOCUMENTER_NEWEST = "v0.8.4";
+var DOCUMENTER_STABLE = "stable";
